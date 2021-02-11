29ba273a9e99db184849b126112dec15c202e56f
Qlou895 <qluis895@gmail.com>
2021-02-11 17:36:54 -0500

    made changes to the search.html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-var doc_div = document.querySelector("#stuff");
+//this file will have the functions for collecting the data from the search.htlm and dislplay the results to the user.
+
+
+var doc_div = document.querySelector("#menuList");
 var btn = document.querySelector("#searchButton");
 
 //async means do your thing in the background so everything can continue to work
-async function generateData(){
-    //fetch data
-    var response = await fetch("https://www.boredapi.com/api/activity");
+async function generateData() {
+    
+    //response waits for the data to be fetched from the api
+    var response = await fetch("https://www.boredapi.com/api/activity, { method: 'POST', }");//idk if this will work
+
+    //converts the raw data to a javaScript obj
     var data = await response.json();
     console.log(data);
     
@@ -13,3 +19,9 @@ async function generateData(){
 }
 
 btn.addEventListener('click', generateData);
+
+/********************************* note to self *****************************
+
+    Do I want to use JQuery?
+    Turn the form into a obj?
+*/
