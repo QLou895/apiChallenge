e4109a790b3b7db2676a84c640b5772ca0616820
Shelzer101 <35784457+Shelzer101@users.noreply.github.com>
2021-02-18 23:06:08 -0500

    stuff

--- js/topPicks.js
+++ js/topPicks.js
@@ -1,13 +1,13 @@
 // generatePick("activity3", 5);
 //async means do your thing in the background so everything can continue to work
+var doc_div = document.getElementById('activity3');
 
-
-async function generatePick(mydiv, num){
+async function generatePick(num){
 
     var keys = [3621244, 8779876, 6613428, 5881647, 8092359];
 
-    var div = document.getElementById(mydiv);
-    console.log(mydiv);
+    // var doc_div = document.getElementById('activity3');
+    // console.log(doc_div.innerText);
     num -= 1;
     mykey = keys[num];
 
@@ -18,6 +18,8 @@ async function generatePick(mydiv, num){
 
     
     const typeUpper = data.type[0].toUpperCase() + data.type.substring(1);
-    div.innerHTML = "Activity: " + data.activity + "<br>Accessibility: " + data.accessibility + "<br>Type: " + typeUpper + "<br>Participants: " + data.participants + "<br>Price: " + data.price;
+    doc_div.innerHTML = "Activity: " + data.activity + "<br>Accessibility: " + data.accessibility + "<br>Type: " + typeUpper + "<br>Participants: " + data.participants + "<br>Price: " + data.price;
 
 }
+
+generatePick(5);
