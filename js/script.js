cfe58cea567e6566dd5ff3ea9cfc7197757cb092
Shelzer101 <35784457+Shelzer101@users.noreply.github.com>
2021-02-19 02:10:17 -0500

    final

--- js/script.js
+++ js/script.js
@@ -59,3 +59,22 @@ function relabelData(dataName, dataValue){
     return label;
 
 }
+
+async function generatePick(mydiv, num){
+
+    var keys = [3621244, 8779876, 6613428, 5881647, 8092359];
+
+    var div = document.getElementById(mydiv);
+    num -= 1;
+    mykey = keys[num];
+
+    var response = await fetch("https://www.boredapi.com/api/activity?key=" + mykey);
+    var data = await response.json();
+    console.log(data);
+
+
+    
+    const typeUpper = data.type[0].toUpperCase() + data.type.substring(1);
+    div.innerHTML = "<strong>Activity: </strong>" + data.activity + "<br><strong>Accessibility: </strong>" + relabelData(1, data.accessibility) + "<br><strong>Type: </strong>" + typeUpper + "<br><strong>Participants: </strong>" + data.participants + "<br><strong>Price: </strong>" + relabelData(0, data.price);
+
+}
